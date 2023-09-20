83a85f62eeca1ece4982053bb7026b59b917a14a
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-20 14:24:58 -0300

    class 124 - entering numbers on the display

--- calculadora_simples/assets/js/app.js
+++ calculadora_simples/assets/js/app.js
@@ -4,7 +4,7 @@
 const operators_button = document.querySelectorAll('[data_operator]');
 const numbers_button = document.querySelectorAll('[data_number]');
 const equal_button = document.querySelector('[data_equal]');
-const display = document.querySelector('[data_display]');
+const display = document.querySelector('[data_display_input]');
 const dot_button = document.querySelector('[data_dot]');
 
 /*
@@ -18,7 +18,39 @@ let operator = null;
 /*
     functions
 */
+//
+const updateDisplay = () => {
+    display.value = current_operation;
+};
+
+// 
+const insertNumber = (event) => {
+    if (calculating) {
+        current_operation = event.target.textContent;
+        calculating = false;
+    }
+    else {
+        current_operation += event.target.textContent;
+    };
+
+    updateDisplay();
+};
+
+// 
+const insertDot = () => {
+    if (current_operation.indexOf('.') === -1) {
+        current_operation += '.';
+        updateDisplay();
+    }
+};
 
 /*
     event
 */
+//
+dot_button.addEventListener('click', insertDot);
+
+// 
+numbers_button.forEach((number) => {
+    number.addEventListener('click', insertNumber);
+});
